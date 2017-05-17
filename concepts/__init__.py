1c67dbd3a540d387921412b28c777eee6f7afb4f
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-05-17 17:52:33 +0200

    bump version for development

--- concepts/__init__.py
+++ concepts/__init__.py
@@ -8,7 +8,7 @@ from .definitions import Definition
 __all__ = ['Context', 'Definition', 'load_cxt', 'load_csv', 'make_context']
 
 __title__ = 'concepts'
-__version__ = '0.7.11'
+__version__ = '0.7.12.dev0'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE'
 __copyright__ = 'Copyright (c) 2013-2016 Sebastian Bank'
