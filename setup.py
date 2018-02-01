d5c9a1aa090d64759db25d57e036a91b5f3931a3
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-02-01 15:25:38 +0100

    upgrade pytest to 3.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     extras_require={
         'dev': ['flake8', 'pep8-naming', 'wheel', 'twine'],
-        'test': ['pytest>=3.3', 'pytest-cov'],
+        'test': ['pytest>=3.4', 'pytest-cov'],
         'docs': ['sphinx>=1.3', 'sphinx-rtd-theme'],
     },
     long_description=io.open('README.rst', encoding='utf-8').read(),
