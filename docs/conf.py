c15ddcb9d95ae38b53a78a4a3e79688e4caca1d8
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-05-25 19:07:32 +0200

    bump version for development

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ author = u'Sebastian Bank'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.7.13'
+version = u'0.7.14.dev0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
