f46e6704bc480a4b5ea0ba1a3acea3e1ed7be6b9
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-02-03 13:08:28 +0100

    bump to next version for added methods

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='concepts',
-    version='0.7.15.dev0',
+    version='0.8.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Formal Concept Analysis with Python',
