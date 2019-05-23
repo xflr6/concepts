3942e5e6b3c77557993892f27d5d3e34ded69a2a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-05-23 11:32:46 +0200

    adapt naming

--- visualize-examples.py
+++ visualize-examples.py
@@ -3,7 +3,7 @@
 
 import concepts.visualize
 
-ARGS = {'directory': 'visualize-output', 'out_format': 'pdf'}
+KWARGS = {'directory': 'visualize-output', 'out_format': 'pdf'}
 
-concepts.visualize.render_all('examples/*.cxt', **ARGS)
-concepts.visualize.render_all('examples/[r-v]*.csv', encoding='utf-8', **ARGS)
+concepts.visualize.render_all('examples/*.cxt', **KWARGS)
+concepts.visualize.render_all('examples/[r-v]*.csv', encoding='utf-8', **KWARGS)
