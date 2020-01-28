00d15e466d7513e9021776e8526e621da33f163d
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-01-28 18:17:25 +0100

    add project_urls for PyPI links

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setup(
     keywords='fca complete lattice graph join meet galois',
     license='MIT',
     url='https://github.com/xflr6/concepts',
+    project_urls={
+        'Documentation': 'https://concepts.readthedocs.io',
+        'Changelog': 'https://concepts.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/xflr6/concepts/issues',
+    },
     packages=find_packages(),
     platforms='any',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
