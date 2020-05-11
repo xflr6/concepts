89a4a1ae5e6e31d8a1393d1dd731d989ab45a75f
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-05-11 15:02:23 +0200

    upgrade docs extra for sphinx>1.8, add tox as dev extra

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
         'graphviz~=0.7',
     ],
     extras_require={
-        'dev': ['flake8', 'pep8-naming', 'wheel', 'twine'],
-        'test': ['pytest>=4.6,!=3.10.0', 'pytest-cov'],
-        'docs': ['sphinx>=1.7', 'sphinx-rtd-theme'],
+        'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['pytest>=4.6', 'pytest-cov'],
+        'docs': ['sphinx>=1.8', 'sphinx-rtd-theme'],
     },
     long_description=io.open('README.rst', encoding='utf-8').read(),
     classifiers=[
