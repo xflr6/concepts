71eb8f5879a7ee6db060347c6ded20649f8a2e59
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-04-18 20:03:40 +0200

    move Concept out to concepts.py

--- concepts/algorithms.py
+++ concepts/algorithms.py
@@ -1,7 +1,8 @@
 import functools
 import heapq
 
-__all__ = ['lattice', 'neighbors']
+__all__ = ['lattice', 'neighbors',
+           'iterunion']
 
 
 def lattice(Extent, *, infimum):
@@ -54,3 +55,24 @@ def neighbors(objects, *, Extent):
             minimal &= ~add
         else:
             yield extent, intent
+
+
+def iterunion(concepts, sortkey, next_concepts):
+    heap = [(sortkey(c), c) for c in concepts]
+    heapq.heapify(heap)
+
+    push = functools.partial(heapq.heappush, heap)
+    pop = functools.partial(heapq.heappop, heap)
+
+    seen = -1
+
+    while heap:
+        index, concept = pop()
+        # requires sortkey to be an extension of the lattice order
+        # (a toplogical sort of it) in the direction of next_concepts
+        # assert index >= seen
+        if index > seen:
+            seen = index
+            yield concept
+            for c in next_concepts(concept):
+                push((sortkey(c), c))
