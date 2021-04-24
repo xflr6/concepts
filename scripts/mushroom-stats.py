d2e7cf8f8dfa6547b562107c0fcbaaa77cfc557f
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-04-25 00:29:57 +0200

    add mushroom stats calculation script

---
+++ scripts/mushroom-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import itertools
+
+import pandas as pd
+
+from mushroom import MUSHROOM, ENCODING, CSV, properties
+
+
+dtype = {MUSHROOM.stem: 'uint'}
+dtype.update((p, bool) for p in properties)
+
+df = pd.read_csv(CSV, dtype=dtype, index_col=MUSHROOM.stem, encoding=ENCODING)
+df.info(memory_usage='deep')
+
+empty = df.any(axis='rows')[lambda x: ~x].index.tolist()
+print(empty)
+assert len(empty) == 9
+df = df.drop(empty, axis='columns')
+df.info(memory_usage='deep')
+
+cf = (df[['class:edible', 'class:poisonous']]
+      .sum().to_frame('count')
+      .assign(ratio=lambda x: 100 * x / x.sum()))
+
+cf = pd.concat([cf, cf.sum().to_frame('total').T])
+
+print(cf)
+print('fill ratio:', f'{df.sum().sum() / df.count().sum():.2%}')
