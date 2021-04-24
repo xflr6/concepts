ac9e8d99f375a100dabe2094c56ad8a22ac3921a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-04-24 10:44:56 +0200

    fix incorrect test name

--- tests/slow/test_algorithms_slow.py
+++ tests/slow/test_algorithms_slow.py
@@ -8,24 +8,23 @@ import concepts
 ENCODING = 'utf-8'
 
 
-def test_fcbo_mushroom(test_output, test_examples, filename='bob_ross.cxt'):
+def test_lattice_bob_rows(test_output, test_examples, filename='bob_ross.cxt'):
     filename = pathlib.Path(filename)
     source = test_examples / filename
 
-    print(concepts)
     context = concepts.load_cxt(str(source))
 
     assert len(context.objects) == 403
     assert len(context.properties) == 67
 
     start = time.perf_counter()
+
     lattice = context.lattice
 
     duration = time.perf_counter() - start
     print(duration)
 
     assert lattice is not None
-
     assert len(lattice) == 3_463
 
     target = test_output / f'{filename.stem}-serialized.py'
